73d932d09247e425a985cb636e6ad6b1201e3fc2
Derpipose <thefluffmaster2000@gmail.com>
2024-02-26 17:15:41 -0700

    Updated things and now have a working dbcontext.

---
+++ TheDerpening/Data/ListDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TheDerpening.Data.Models;
+
+namespace TheDerpening.Data {
+    public class ListDbContext: DbContext {
+        public DbSet<TodoListItem> Todos { get; set; }
+
+        public ListDbContext(DbContextOptions<ListDbContext> options) : base(options) {
+        }
+
+    }
+}
---
+++ TheDerpening/Data/Models/TodoListItem.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace TheDerpening.Data.Models
+{
+    public class TodoListItem
+    {
+        public int Id { get; set; }
+        public string ?Title { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+
+    /*public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Todo> Todos { get; set; }
+    }*/
+
+
+}
