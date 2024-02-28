5a9baa6283c5ef88bab949b8c76252deccd184fc
Derpipose <thefluffmaster2000@gmail.com>
2024-02-27 21:25:02 -0700

    Everything should be up and running for dockercompose imaging

--- TheDerpening/init.sql
+++ TheDerpening/init.sql
@@ -1,10 +1,8 @@
 ﻿
-CREATE TABLE [Todos] (
-    [Id] int NOT NULL IDENTITY,
-    [Title] nvarchar(90) NOT NULL,
-    [IsTaskCompleted] bit NOT NULL,
-    CONSTRAINT [PK_Todos] PRIMARY KEY ([Id])
+CREATE TABLE Todos (
+    Id int NOT NULL,
+    Title nvarchar(90) NOT NULL,
+    IsTaskCompleted bit NOT NULL
 );
-GO
 
 
