8cd7bdf6df4e015e2b853c25ef9954e74fbf2c78
Derpipose <thefluffmaster2000@gmail.com>
2024-03-08 13:06:14 -0700

    Attempting new API So this is a clean slate build

---
+++ DerpeningAPI/ToDoItem.cs
@@ -0,0 +1,8 @@
+﻿namespace DerpeningAPI {
+    public class ToDoItem {
+        public int ApiId { get; set; }
+        public string ApiTitle { get; set; }
+        public bool ApiIsTaskCompleted { get; set; }
+
+    }
+}
--- TheDerpening/init.sql
+++ TheDerpening/init.sql
@@ -1,8 +1,25 @@
-﻿
-CREATE TABLE Todos (
+﻿CREATE TABLE Todos (
     Id int NOT NULL,
     Title nvarchar(90) NOT NULL,
     IsTaskCompleted bit NOT NULL
 );
 
+CREATE TABLE Players (
+    Id int NOT NULL,
+    Name nvarchar(60) NOT NULL,
+    Keyword nvarchar(60) NOT NULL,
+    PRIMARY KEY (Id)
+);
 
+CREATE TABLE Characters (
+    Id int NOT NULL,
+    PlayerId int NOT NULL,
+    CharacterName nvarchar(90) NOT NULL,
+    Strength int,
+    Dexterity int,
+    Constitution int,
+    Intelligence int,
+    Wisdom int, 
+    Charisma int,
+    FOREIGN KEY (PlayerId) REFERENCES Players(Id)
+);
